7262a5ef8321837cc52d3a35143b09acfcca236d
Martin Slapak <slapak@cesnet.cz>
2016-12-16 23:19:25 +0100

    Project cleanup.

--- libs.sh
+++ libs.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
 if [ -n "$LD_LIBRARY_PATH" ]; then
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/opencv/lib
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/opencv/lib
+	echo "Pridano, soucasny obsah: $LD_LIBRARY_PATH";
 else
-    export LD_LIBRARY_PATH=`pwd`/opencv/lib
+	export LD_LIBRARY_PATH=`pwd`/opencv/lib
+	echo "Pridana nova polozka: $LD_LIBRARY_PATH";
 fi
